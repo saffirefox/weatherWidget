fa06201744dfcee2c74b313a3ba7530b069800bf
saffirefox <531eis@gmail.com>
2014-08-26 11:07:13 -0600

    Initial commit. Changed formatting.

--- js/weather.js
+++ js/weather.js
@@ -1,30 +1,30 @@
 $(function() {
-	weather = {
+	    weather = {
 		cityID: '',
 		temp: '',
 
-		getWeather: function(id) {
+		getWeather: function(id) {                      //in JSON, each city has an id number
 			
-			if(id === '') {
-				this.cityID = $('#cityList').val();				
+			if(id === '') {                             //if id is empty string (no city selected)
+				this.cityID = $('#cityList').val();     //get value of city selected (#cityList is pull down list of cities)
 			} else {
-				this.cityID = id;
+				this.cityID = id;                       //if a city has already been selected, set id to city number from JSON
 			}
-			// console.log(this.cityID);
 
-			$.ajax({
-				url: 'http://api.openweathermap.org/data/2.5/weather',
-				type: 'GET',
-				dataType: 'json',
-				data: {id: this.cityID},
+
+			$.ajax({                                                    //AJAX call
+				url: 'http://api.openweathermap.org/data/2.5/weather',  //link to openweathermap.org data
+				type: 'GET',                                            //use GET request to retrieve data
+				dataType: 'json',                                       //retrieve info in JSON format rather than XML
+				data: {id: this.cityID}                                 //id in JSON record is a 7 digit number identifying the city
 			})
 			.done(function(data) {
 				// console.log(data);
 				// console.log(data.main.temp);
 
-				weather.postTemp(data.main.temp);
-				weather.changeTown();
-				weather.changeIcon(data.weather[0].icon);
+				weather.postTemp(data.main.temp);                       //"main" is a key in JSON, temp is a value paired to "main" and is in Kelvin units
+				weather.changeTown();                                   //function call
+				weather.changeIcon(data.weather[0].icon);               //
 
 				
 				// console.log("success");
@@ -79,7 +79,7 @@ $(function() {
 			// console.log(icon);
 			$('.icon img').attr('src', "http://openweathermap.org/img/w/"+icon+".png")
 		}
-	}
+	};
 
 	weather.listenCity();
 	weather.listenFormat();
